4585fbcdb03f3c7d754ad9fe4ae4b5976c3f9e53
Caalgato <33143909@uaz.edu.mx>
2026-06-10 16:53:23 -0600

    deleted: Carpeta dev del proyecto

--- dev/Django/uaz_frameworks/core/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Miembro
-
-class MiembroSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Miembro
-        fields = ['id', 'nombre', 'correo', 'cursos', 'foto']
